27d26c11dcdd9f8d3e04b2a8e650b617eda1842d
invitado <invitado@s4pc3.eisc.univalle.edu.co>
2019-07-09 11:57:40 -0500

    adicionando archivo

---
+++ Ejercicios-Programacion-C05/p3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+int rc = fork();
+if (rc < 0) {
+// fork failed
+fprintf(stderr, "fork failed\n");
+exit(1);
+} else if (rc == 0) {
+// child (new process)
+printf("hola\n");
+} else {
+// parent process
+sleep(1);
+printf("adios\n");
+}
+return 0;
+}
