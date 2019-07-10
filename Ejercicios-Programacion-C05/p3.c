ce6dd27d8869cfa5ed54bdfd6d4700de510b6267
brayan-rincones <50997049+brayan-rincones@users.noreply.github.com>
2019-07-09 23:03:49 -0500

    Update p3.c

--- Ejercicios-Programacion-C05/p3.c
+++ Ejercicios-Programacion-C05/p3.c
@@ -7,17 +7,17 @@ int main(int argc, char *argv[]) {
 int rc = fork();
   
 if (rc < 0) {
-// fork failed
+  // fork failed
   fprintf(stderr, "fork failed\n");
   exit(1);
   
   } else if (rc == 0) {
-// child (new process)
+  // child (new process)
   printf("hola\n");
   }
   
   else {
-// parent process
+  // parent process
   sleep(1);
   printf("adios\n");
   }
