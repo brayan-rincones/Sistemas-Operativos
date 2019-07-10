f373251ba2bd773fa19be7043997a182fece6e3e
brayan-rincones <50997049+brayan-rincones@users.noreply.github.com>
2019-07-09 22:34:27 -0500

    Update p2.c

--- Ejercicios-Programacion-C05/p2.c
+++ Ejercicios-Programacion-C05/p2.c
@@ -13,16 +13,20 @@ if (rc < 0) {
 // fork failed
 fprintf(stderr, "fork failed\n");
 exit(1);
+  
 } else if (rc == 0) {
-const char * texto_hijo = "texto que se imprime por parte de hijo\n";
-printf("proceso hijo escribio el archivo\n");
-write(archivo, texto_hijo, strlen(texto_hijo));
-exit(0);
-} else {
-wait(NULL);
-const char * texto_padre = "texto imprimido por parte de padre\n";
-printf("proceso padre escribio el archivo\n");
-write(archivo, texto_padre, strlen(texto_padre));
-}
+  const char * texto_hijo = "texto que se imprime por parte de hijo\n";
+  printf("proceso hijo escribio el archivo\n");
+  write(archivo, texto_hijo, strlen(texto_hijo));
+  exit(0);
+  } 
+  
+  else {
+  wait(NULL);
+  const char * texto_padre = "texto imprimido por parte de padre\n";
+  printf("proceso padre escribio el archivo\n");
+  write(archivo, texto_padre, strlen(texto_padre));
+  }
+  
 return 0;
 }
