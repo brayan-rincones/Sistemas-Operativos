3313ad8873fe104fdd319a76ff28f4eab523630b
brayan-rincones <50997049+brayan-rincones@users.noreply.github.com>
2019-07-31 15:38:20 -0500

    Update principal.c

--- 2019_05_31/principal.c
+++ 2019_05_31/principal.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "entero.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv){
   entero a,b;
 
-  a = 5;
-  b = 6;
-  printf("la division de %d / %d es %f\n",a,b,(float)divi(a,b));
-
+  a=10;
+  b=6;
+  printf("la suma de %d + %d es %d\n",a,b,(int)suma(a,b));
+  printf("la resta de %d - %d es %d\n",a,b,(int)resta(a,b));
+  printf("la multiplicacion de %d * %d es %d\n",a,b,(int)multi(a,b));
+  printf("la suma de %d / %d es %f\n",a,b,(float)divi(a,b));
 }
+
 
