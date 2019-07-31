32b7eea8b8615424d6445ac204c6cae22b3f80c9
brayan-rincones <50997049+brayan-rincones@users.noreply.github.com>
2019-07-31 15:36:37 -0500

    Update entero.h

--- 2019_05_31/entero.h
+++ 2019_05_31/entero.h
@@ -2,9 +2,10 @@
 #define __entero__
 
 #define entero int
-#define suma(a,b) a + b
-#define resta(a,b) a - b
-#define divi (a,b) a / b
-#define multi(a,b) b * a
+#define suma(a,b) a+b
+#define resta(a,b) a-b
+#define multi(a,b) a*b
+#define divi(a,b) a/b
+
 #endif
 
